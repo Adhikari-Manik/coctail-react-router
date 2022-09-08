3c133af191d519a78c9ee170becaa9c3c3be47ed
Adhikari-Manik <manikadhikari10@gmail.com>
2022-09-08 15:18:03 +1000

    react router

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum vero
+        nostrum beatae sunt voluptate tenetur non ut architecto quis, eos
+        doloribus ea, consequuntur quos sint labore esse? Exercitationem, aut
+        distinctio fugiat veritatis mollitia ut non odit, minus commodi pariatur
+        ipsam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
